57e85b1a2d79211bf3c6af7892095e69d15709ec
Adam Recvlohe <adam_recvlohe@valpak.com>
2016-01-05 16:07:40 -0500

    updated with react router

---
+++ src/ListingPage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import styles from './style.scss';
+import Coupon from './Coupon';
+
+class ListingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listing: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchListing();
+  }
+
+  render() {
+    console.log(this.state.listing);
+    const styles = {
+
+    };
+
+    return (
+      <div>
+        <Coupon coupon={this.state.listing} />
+      </div>
+    );
+  }
+
+  fetchListing() {
+    fetch(`https://apidev.valpak.com/pub/offers/valpak/listings/${this.props.params.cid}`)
+      .then(response => {
+        response.json().then(json => {
+          this.setState({
+            listing: json[0]
+          });
+        });
+      });
+  }
+}
+
+export default ListingPage;
